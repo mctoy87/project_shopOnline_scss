106ce9876a74dc25f711f8ea208d74b75d2b9999
mctoy87 <mctoy87@mail.ru>
2023-10-18 19:21:21 +0200

    minified html, css, js, img

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,44 @@ import browserSync from 'browser-sync';
 import sassPkg from 'sass';
 import gulpSass from 'gulp-sass';
 import gulpCssimport from 'gulp-cssimport';
-import {deleteAsync} from 'del'; 
+import {deleteAsync} from 'del';
+import htmlmin from 'gulp-htmlmin'; 
+import cleanCSS from 'gulp-clean-css';
+import gulpif from 'gulp-if';
+import rename from 'gulp-rename';
+import plumber from 'gulp-plumber';
+import terser from 'gulp-terser';
+import webpackStream from 'webpack-stream';
+import webpack from 'webpack';
+import sourcemaps from 'gulp-sourcemaps';
+import gulpImg from 'gulp-image';
+import gulpWebp from 'gulp-webp'
+import gulpAvif from 'gulp-avif';
+import { stream as critical} from 'critical';
 
 const prepros = true;
+
+let dev = false;
+
 const sass = gulpSass(sassPkg);
+
+const allJS = [
+  'src/script/modules/timer.js',
+  'src/script/modules/menu.js',
+  'src/script/modules/blog.js',
+  'src/script/modules/article.js',
+];
+
 
 // задачи
 
 
 export const html = () => gulp
   .src('src/*.html')
+  .pipe(htmlmin({
+    removeComments: true,
+    collapseWhitespace: true,
+  }))
   .pipe(gulp.dest('dist'))
   .pipe(browserSync.stream()); //событие - перезагрузка страницы на слушателя в server
 
@@ -22,31 +50,120 @@ export const style = () => {
   if(prepros) {
     return gulp
       .src('src/scss/**/*.scss')
+      .pipe(gulpif(dev, sourcemaps.init()))
       .pipe(sass().on('error', sass.logError))
+      .pipe(cleanCSS({
+        2: {
+          specialComments: 0,
+        }
+      }))
+      .pipe(gulpif(dev,sourcemaps.write('../maps')))
       .pipe(gulp.dest('dist/css'))
       .pipe(browserSync.stream());
   }
 
   return gulp
     .src('src/css/index.css')
+    .pipe(gulpif(dev, sourcemaps.init()))
     .pipe(gulpCssimport({
       extensions: ["css"], // process only css
     }))
+    .pipe(cleanCSS({
+      2: {
+        specialComments: 0,
+      }
+    }))
+    .pipe(gulpif(dev,sourcemaps.write('../maps')))
     .pipe(gulp.dest('dist/css'))
     .pipe(browserSync.stream());
 }
 
+// js
+
+const webpackConf = {
+  mode: dev ? 'development' : 'production',
+  devtool: dev ? 'eval-source-map' : false,
+  optimization: {
+    minimize: false,
+  },
+  output: {
+    filename: 'index.js',
+  },
+  module: {
+    rules: [],
+  },
+};
+
+if (!dev) {
+  webpackConf.module.rules.push({
+    test: /\.(js)$/,
+    exclude: /(node_modules)/,
+    loader: 'babel-loader',
+  });
+}
+
 export const js = () => gulp
-  .src('src/script/**/*.js')
+  .src([...allJS, 'src/script/**/*.js'])
+  .pipe(gulpif(dev, sourcemaps.init()))
+  .pipe(plumber())
+  .pipe(webpackStream(webpackConf, webpack))
+  .pipe(gulpif(!dev, gulp.dest('dist/script')))
+  .pipe(gulpif(!dev, terser()))
+  .pipe(
+    rename({
+      suffix: '.min',
+    }),
+  )
+  .pipe(gulpif(dev,sourcemaps.write('../maps')))
   .pipe(gulp.dest('dist/script'))
   .pipe(browserSync.stream());
 
+export const img = () => gulp
+  .src('src/images/**/*.{jpg, jpeg, png, svg, gif}')
+  .pipe(gulpif(!dev, gulpImg({
+    optipng: ['-i 1', '-strip all', '-fix', '-o7', '-force'],
+    pngquant: ['--speed=1', '--force', 256],
+    zopflipng: ['-y', '--lossy_8bit', '--lossy_transparent'],
+    jpegRecompress: ['--strip', '--quality', 'medium', '--min', 40, '--max', 80],
+    mozjpeg: ['-optimize', '-progressive'],
+    gifsicle: ['--optimize'],
+    svgo: true,
+  })))
+  .pipe(gulp.dest('dist/images'))
+  .pipe(browserSync.stream());
+
+export const webp = () => gulp
+  .src('src/images/**/*.{jpg, jpeg, png}')
+  .pipe(gulpWebp({
+    quality: 60,
+  }))
+  .pipe(gulp.dest('dist/images'))
+  .pipe(browserSync.stream());
+
+  export const avif = () => gulp
+  .src('src/images/**/*.{jpg, jpeg, png}')
+  .pipe(gulpAvif({
+    quality: 50,
+  }))
+  .pipe(gulp.dest('dist/images'))
+  .pipe(browserSync.stream());
+
+
+export const critCSS = () => gulp
+  .src('dist/*.html')
+  .pipe(critical({
+    base: 'dist',
+    inline: true,
+    css: ['dist/css/index.css']
+  }))
+  .on('error', err => {
+    console.error(err.message);
+  })
+  .pipe(gulp.dest('dist'));
+
 export const copy = () => gulp
-  .src([
-    'src/fonts/**/*',
-    'src/images/**/*'
-  ], {
-  base: 'src' //отслеживание путей файлов чтобы их складывать также
+  .src('src/fonts/**/*', {
+    base: 'src' //отслеживание путей файлов чтобы их складывать также
   })
   .pipe(gulp.dest('dist')) //кладем его в dev
   .pipe(browserSync.stream({
@@ -66,8 +183,11 @@ export const server = () => {
   // слушатели на изменения в любых файлах
   gulp.watch('./src/**/*.html', html);
   gulp.watch(prepros ? './src/scss/**/*.scss' : './src/css/**/*.css', style);
+  gulp.watch('src/images/**/*.{jpg, jpeg, png, svg, gif}', img);
   gulp.watch('./src/script/**/*.js', js);
-  gulp.watch(['./src/images/**/*', 'src/fonts/**/*'], copy);
+  gulp.watch('src/fonts/**/*', copy);
+  gulp.watch('src/images/**/*.{jpg, jpeg, png}', webp);
+  gulp.watch('src/images/**/*.{jpg, jpeg, png}', avif);
 }
 //очищает dist для dev и build
 export const clear = (done) => {
@@ -80,9 +200,14 @@ export const clear = (done) => {
   // Запуск
 
   // повторяющиеся базовые операции
-export const base = gulp.parallel(html, style, js, copy);
+
+export const develop = async() => {
+  dev = true;
+}
+
+export const base = gulp.parallel(html, style, js, img, avif, webp, copy);
 
   // сборка build
-export const build = gulp.series(clear, base);
+export const build = gulp.series(clear, base, critCSS);
 // сборка dev
-export default gulp.series(base, server);
+export default gulp.series(develop, base, server);
